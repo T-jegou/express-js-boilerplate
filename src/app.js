f340e42b3c744531c8f0d8c54f3cece3ca8dcba9
T-jegou <thomas.jegou1@gmail.com>
2023-10-30 15:34:34 +0100

    add static listening port

--- src/app.js
+++ src/app.js
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(process.env.PORT, () =>
+app.listen(3000, () =>
   console.log('Example app listening on port 3000!'),
 );
